9ba2db3bce28dc32811ba687669e97d9fdd40020
nguyendang092 <trungnguyencv092@gmail.com>
2025-09-13 09:10:54 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // quan trọng khi build để deploy trên server
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // dùng @ để import src dễ dàng
-    },
-  },
-  build: {
-    outDir: 'dist', // thư mục build
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
-    open: true,
-    strictPort: true,
-    // historyApiFallback: true // chỉ cần nếu dev server muốn fallback route
-  }
-});
+  base: "/",
+})
